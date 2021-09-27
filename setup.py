500c8ca9c6329f9c6397aaef939c8c4e06aba446
David Wade <dawad@equinor.com>
2021-09-27 12:03:18 +0200

    Use SCM version

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 import setuptools
 
+
 setuptools.setup(name='zgyio',
                  author='equinor',
                  description='Convenience wrapper around OpenZGY Python implementation',
                  long_description='Convenience wrapper around OpenZGY Python implementation which enables reading ZGY files with a syntax familiar to users of segyio.',
                  url='https://github.com/equinor/zgyio',
 
-                 version='0.0.0',
+                 use_scm_version=True,
                  install_requires=['numpy'],
-                 setup_requires=['setuptools'],
+                 setup_requires=['setuptools', 'setuptools_scm'],
                  packages=['zgyio', 'openzgy']
                  )
